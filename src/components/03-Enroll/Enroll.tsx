3325c3f1169684f4b1cf585ce0207b2317fb0643
IONHAX04 <thirukumara04@gmail.com>
2024-11-21 09:55:00 +0530

    Enroll drafted

--- src/components/03-Enroll/Enroll.tsx
+++ src/components/03-Enroll/Enroll.tsx
@@ -1,10 +1,26 @@
 import { IonContent, IonPage } from "@ionic/react";
 import React from "react";
 
+import "./Enroll.css";
+
 const Enroll: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen></IonContent>
+      <IonContent fullscreen>
+        <div className="signinPage">
+          <p className="signinHeader">Sign In</p>
+          <p className="welcomeCont">Hi! Welcome Back, You've Been Missed !!</p>
+
+          <div className="formContentSignIn">
+            <p>Email</p>
+            <input type="email" required />
+            <p>Password</p>
+            <input type="password" required />
+            <p className="forgotPassword">Forgot Password ?</p>
+            <button>Sign In</button>
+          </div>
+        </div>
+      </IonContent>
     </IonPage>
   );
 };
